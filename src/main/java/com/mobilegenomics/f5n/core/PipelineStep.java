ed1bc1794a638ac7f8dbf2d812efb7022dcd75fe
AnjanaSenanayake <anjanajayabandara@gmail.com>
2019-12-03 14:07:27 +0530

    pipeline name attribute added

--- src/main/java/com/mobilegenomics/f5n/core/PipelineStep.java
+++ src/main/java/com/mobilegenomics/f5n/core/PipelineStep.java
@@ -3,20 +3,23 @@ package com.mobilegenomics.f5n.core;
 public enum PipelineStep {
     //    MINIMAP2_SEQUENCE_ALIGNMENT(0,
 //        "minimap2 -x map-ont -a /mnt/sdcard/f5c/test/ecoli_2kb_region/draft.fa /mnt/sdcard/f5c/test/ecoli_2kb_region/reads.fasta"),
-    MINIMAP2_SEQUENCE_ALIGNMENT(0, "minimap2 -x map-ont"),
-    SAMTOOL_SORT(1, "samtool sort"),
-    SAMTOOL_INDEX(2, "samtool index"),
-    F5C_INDEX(3, "f5c index"),
-    F5C_CALL_METHYLATION(4, "f5c call-methylation --secondary=yes --min-mapq=0 -B 2M"),
-    F5C_EVENT_ALIGNMENT(5, "f5c eventalign");
+    MINIMAP2_SEQUENCE_ALIGNMENT(0, "minimap2 -x map-ont", "Minimap2 Sequence Alignment"),
+    SAMTOOL_SORT(1, "samtool sort", "Samtool Sort"),
+    SAMTOOL_INDEX(2, "samtool index", "Samtool Index"),
+    F5C_INDEX(3, "f5c index", "F5C Index"),
+    F5C_CALL_METHYLATION(4, "f5c call-methylation --secondary=yes --min-mapq=0 -B 2M", "F5C Call Methylation"),
+    F5C_EVENT_ALIGNMENT(5, "f5c eventalign", "F5C Event Alignment");
 
     private final int value;
 
     private final String command;
 
-    PipelineStep(int value, String command) {
+    private final String attribute;
+
+    PipelineStep(int value, String command, String attribute) {
         this.value = value;
         this.command = command;
+        this.attribute = attribute;
     }
 
     public int getValue() {
@@ -25,5 +28,9 @@ public enum PipelineStep {
 
     public String getCommand() {
         return command;
+    }
+
+    public String getAttribute() {
+        return attribute;
     }
 }
