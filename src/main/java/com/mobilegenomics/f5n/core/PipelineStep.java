a867d37eac44c06ebeef24b36e43f9e355a991d9
AnjanaSenanayake <anjanajayabandara@gmail.com>
2019-12-03 14:13:00 +0530

    pipleline name attribute modified

--- src/main/java/com/mobilegenomics/f5n/core/PipelineStep.java
+++ src/main/java/com/mobilegenomics/f5n/core/PipelineStep.java
@@ -6,9 +6,9 @@ public enum PipelineStep {
     MINIMAP2_SEQUENCE_ALIGNMENT(0, "minimap2 -x map-ont", "Minimap2 Sequence Alignment"),
     SAMTOOL_SORT(1, "samtool sort", "Samtool Sort"),
     SAMTOOL_INDEX(2, "samtool index", "Samtool Index"),
-    F5C_INDEX(3, "f5c index", "F5C Index"),
-    F5C_CALL_METHYLATION(4, "f5c call-methylation --secondary=yes --min-mapq=0 -B 2M", "F5C Call Methylation"),
-    F5C_EVENT_ALIGNMENT(5, "f5c eventalign", "F5C Event Alignment");
+    F5C_INDEX(3, "f5c index", "f5c Index"),
+    F5C_CALL_METHYLATION(4, "f5c call-methylation --secondary=yes --min-mapq=0 -B 2M", "f5c Call Methylation"),
+    F5C_EVENT_ALIGNMENT(5, "f5c eventalign", "f5c Event Alignment");
 
     private final int value;
 
